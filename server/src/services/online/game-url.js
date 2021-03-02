9ff891ef9f42ca4f3f52dd7fa6628f812c9518a5
Hoang <danhhoang5920@gmai.com>
2021-03-02 14:47:34 +0700

    add createURL function

--- server/src/services/online/game-url.js
+++ server/src/services/online/game-url.js
@@ -1,6 +1,15 @@
 const createGameUrl = () => {
   // Create a random string that matches ^[a-zA-Z0-9]{8}$
-  // TODO
+  let string = "";
+  const characters =
+    "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789";
+
+  for (let i = 0; i < 8; i++) {
+    const index = Math.floor(Math.random() * characters.length);
+    const char = characters.charAt(index);
+    string += characters.charAt(index);
+  }
+  return string;
 };
 
 module.exports = {
